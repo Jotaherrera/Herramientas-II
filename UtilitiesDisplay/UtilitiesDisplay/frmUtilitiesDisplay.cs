ffe6541f4d4f766c9ddf3940c2633054b1272c14
Jotaherrera <juanherreramen13@gmail.com>
2022-10-06 21:47:00 -0500

    Display modified

--- UtilitiesDisplay/UtilitiesDisplay/frmUtilitiesDisplay.cs
+++ UtilitiesDisplay/UtilitiesDisplay/frmUtilitiesDisplay.cs
@@ -20,7 +20,6 @@ namespace UtilitiesDisplay
         public frmUtilitiesDisplay()
         {
             InitializeComponent();
-            grpSystem.Focus();
             lblDrives.Text = "";
             lblSerialNumber.Text = "";
             lblLogProcessors.Text = "";
